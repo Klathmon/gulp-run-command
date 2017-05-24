d7cd0d56cf07ad2213d6365de4cb73696b6ddbf4
Gregory Benner <gregbenner1@gmail.com>
2017-05-24 14:04:21 -0400

    buncha changes

--- index.js
+++ index.js
@@ -5,42 +5,23 @@ import spawnArgs from 'spawn-args'
 
 const timedOutSymbol = Symbol('TIMED_OUT')
 
-export default function run (command = [], options = {}) {
+export default function run (command, optionsOrArgs = {}, options = {}) {
   // Expand and fill in the defaults for the options
   const {
     quiet = false,
     ignoreErrors = false,
-    cwd = process.cwd(),
-    timeout = undefined, // In milliseconds
+    cwd = process.cwd()
     env = {}
-  } = options
+  } = (Array.isArray(optionsOrArgs) ? options : optionsOrArgs)
 
-  // Ensure that commands is an array
-  const commands = (Array.isArray(command) ? command : [command])
-
-  // Returning an async function lets gulp know it's complete when the promise resolves
-  return async () => {
-    // Loop over each command and run them one-at-a-time
-    for (let cmd of commands) {
-      // Wrap it in a try/catch to ignore errors if ignoreErrors is set
-      try {
-        // Run the command and add the promise to the promise array
-        const promiseArray = [runCommand(cmd, quiet, pathResolve(cwd), env)]
-        // If timeout is set, then add that as a timebomb to the promise array
-        if (typeof timeout !== 'undefined') promiseArray.push(promiseTimeout(parseInt(timeout, 10), timedOutSymbol))
-
-        // Finally, await on a race between the promises to deal with the first of them
-        const promiseResult = await Promise.race(promiseArray)
-
-        // If the promise resolves with timedOutSymbol first, then throw an error
-        if (promiseResult === timedOutSymbol) {
-          throw new Error('Process timed out...')
-        }
-      } catch (err) {
-        // Unless ignoreErrors is true, re-throw the caught error
-        if (!ignoreErrors) {
-          throw err
-        }
+    // Wrap it in a try/catch to ignore errors if ignoreErrors is set
+    try {
+      // Run the command
+      return runCommand(cmd, quiet, pathResolve(cwd), env)
+    } catch (err) {
+      // Unless ignoreErrors is true, re-throw the caught error
+      if (!ignoreErrors) {
+        throw err
       }
     }
   }
@@ -75,7 +56,7 @@ const runCommand = (command, quiet, cwd, env) => new Promise((resolve) => {
     if (parseInt(code, 10) === 0) {
       resolve()
     } else {
-      throw new Error(`Non-zero exit code of "${code}"`)
+      reject(code)
     }
   })
 })
